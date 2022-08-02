bd4243f88cb18d119af38819f89752e5b2e84587
newbpydev <newbpydev@gmail.com>
2022-08-02 09:16:01 -0300

    finished testing rwv3

---
+++ src/views/EventList.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+
+// import axios from 'axios'
+import EventService from '@/services/EventService'
+
+export default {
+  name: 'EventList',
+
+  components: {
+    EventCard
+  },
+
+  data() {
+    return {
+      events: null
+    }
+  },
+
+  created() {
+    // axios
+    //   .get(
+    //     'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3/events'
+    //   )
+    EventService.getEvents()
+      .then(response => (this.events = response.data))
+      .catch(error => console.log(error))
+  }
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
